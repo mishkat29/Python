198fc0b8fd8b6b8323421135e78124136efc08a4
Mishkat Ahmad Neyazi <49838423+mishkat29@users.noreply.github.com>
2020-11-21 13:34:09 +0530

    Update Date_checker.py

--- Date_checker.py
+++ Date_checker.py
@@ -1,7 +1,7 @@
 import datetime
 
 def days_in_month(year,month):
-    if(year>+datetime.MINYEAR and year<=datetime.MAXYEAR):
+    if(year>=datetime.MINYEAR and year<=datetime.MAXYEAR):
         if(month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12):
             return 31
         elif(month==4 or month==6 or month==9 or month==11):
