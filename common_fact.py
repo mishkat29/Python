7094661eef1c197927eddb8de49eee863ff5a651
Mishkat Ahmad Neyazi <49838423+mishkat29@users.noreply.github.com>
2021-06-16 13:15:59 +0530

    Update common_fact.py

--- common_fact.py
+++ common_fact.py
@@ -36,6 +36,6 @@ while True:
         b = int(input("Please enter b: "))
         break
     except ValueError:
-        print("Oops!  That was no valid number.  Try again...")
+        print("Oops!  That was not a valid number.  Try again...")
 
 print(checker(a,b))
